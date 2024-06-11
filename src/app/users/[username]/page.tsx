4468233e656fa0dc2e3c87bd24a6f373975e54a2
isimmons <isimmons33@gmail.com>
2024-06-11 00:21:06 -0400

    style fixes

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function UsersPage({ params }: Props) {
   return (
-    <div className="container mb-48 mt-36 border-4 border-green-500">
+    <div className="container mx-auto mb-48 mt-36 border-4 border-green-500">
       <h1 className="text-h1">{params.username}</h1>
       <Link href={`/users/${params.username}/notes`}>Notes</Link>
     </div>
